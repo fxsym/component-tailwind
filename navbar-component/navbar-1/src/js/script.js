ccf9813f0b38383aec11215994db6eb83ab47d90
fxsym <fsyamsudin24@gmail.com>
2025-03-26 08:46:23 +0700

    navbar-1, navbar-component. Navbar versi mobile sudah sesuai seperti yang di inginkan, muncul dari atas dengan position relative

--- navbar-component/navbar-1/src/js/script.js
+++ navbar-component/navbar-1/src/js/script.js
@@ -2,9 +2,9 @@ console.log('OK')
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#navMenu')
 const navMenuContain = document.querySelector('#navMenuContain')
+const coba = document.querySelector('#coba')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('top-[100%]'); 
-    navMenu.classList.toggle('top-[-220%]');
+    navMenu.classList.toggle('max-h-100');
 })
